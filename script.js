af6b06611d01a9b9e1dfa876e863498583b3f46c
ebtaterbug <ityjoseph@gmail.com>
2021-10-24 14:50:08 -0700

    add alert to display criteria chosen

--- script.js
+++ script.js
@@ -16,53 +16,59 @@ var characterTypes = {
 var characterLength = window.prompt("Type a number of 8 - 128");
 
 
-// When character length is null, < 8, or > 128 repeat prompt
+// Ask again if character length is not valid
 while (characterLength < 8 || characterLength > 128) {
-  characterLength = window.prompt("Type a number of 8 - 128");
+  characterLength = window.prompt("Type a number between 8 - 128");
 }
-
-alert("Character Length: "+characterLength);
 
 // --------------------------------------
 
 alert("Choose character types to add.");
 
 upper = window.prompt("Add uppercase letters to password? Enter yes or no");
-if (upper == "yes") {
+if (upper.toUpperCase() == "YES") {
   var upper = characterTypes.alphabet.toUpperCase();
+  var upperCaseCharacters = "  Uppercase Alphabet: Yes ";
 }
 else {
   var upper = "";
+  var upperCaseCharacters = "  Uppercase Alphabet: No ";
 }
 
 lower = window.prompt("Add lowercase letters to password? Enter yes or no");
-if (lower == "yes") {
+if (lower.toUpperCase() == "YES") {
   var lower = characterTypes.alphabet;
+  var lowerCaseCharacters = " Lowercase Alphabet: Yes ";
 }
 else {
   var lower = "";
+  var lowerCaseCharacters = " Lowercase Alphabet: No ";
 }
 
 numbers = window.prompt("Add numbers to password? Enter yes or no");
-if (numbers == "yes") {
+if (numbers.toUpperCase() == "YES") {
   var numbers = characterTypes.numeric;
+  var numberCharacters = " Numeric Characters: Yes ";
+
 }
 else {
   var numbers = "";
+  var numberCharacters = " Numeric Characters: No ";
 }
 
 special = window.prompt("Add special characters to password? Enter yes or no");
-if (special == "yes") {
+if (special.toUpperCase() == "YES") {
   var special = characterTypes.specialSymbol;
+  var specialCharacters = " Special Characters: Yes ";
 }
 else {
   var special = "";
+  var specialCharacters = " Special Characters: No ";
 }
 
+alert("You have chosen the following criteria for your random password." + " Length: " + characterLength + upperCaseCharacters + lowerCaseCharacters + numberCharacters + specialCharacters + " Use the GENERATE PASSWORD button below to create a secure password");
 
 var ungeneratedPassword = special+numbers+lower+upper;
-
-console.log(special);
 
 
 var generatePassword = function() {
